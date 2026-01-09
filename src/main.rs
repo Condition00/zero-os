3fbafb6b92b362930fe4432dce786f373166de0d
Anant Satya Mohit Kavuru <anant.kavuru@gmail.com>
2026-01-10 01:51:04 +0530

    test setup and exit logic

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,23 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+    exit_qemu(QemuExitCode::Success); //exit if Success
+}
 
 mod vg_buffer;
 //#![feature(asm)]
 //panic implementation
 
 use core::panic::PanicInfo;
-
 //this will be called on kernel panic
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
@@ -14,10 +25,38 @@ fn panic(info: &PanicInfo) -> ! {
     loop {}
 }
 
+//exit qemu
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+}
+
 //overwriting the os entry point
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
     println!("Hello from really really low{}", "!");
-    panic!();
+
+    #[cfg(test)]
+    test_main();
+
+    loop {}
+}
+
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial assertion... ");
+    assert_eq!(1, 2);
+    println!("[ok]");
 }
