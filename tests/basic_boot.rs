09f9169b16cd560c014f93cdd895e65317368155
Anant Satya Mohit Kavuru <anant.kavuru@gmail.com>
2026-01-10 19:28:05 +0530

    tests integrations

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -7,26 +7,18 @@
 use core::panic::PanicInfo;
 use zero::println;
 
-#[unsafe(no_mangle)]
+#[unsafe(no_mangle)] //dont mangle da name
 pub extern "C" fn _start() -> ! {
-    println!("Hello World{}", "!");
-
-    #[cfg(test)]
     test_main();
-
     loop {}
 }
 
-// This function is called on panic.
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
-}
-
-#[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     zero::test_panic_handler(info)
+}
+
+#[test_case]
+fn test_println() {
+    println!("test_println output");
 }
