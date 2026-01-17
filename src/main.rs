bbcb0cf583498d2689a60e9eb99d56d79ee5e7f4
Anant Satya Mohit Kavuru <anant.kavuru@gmail.com>
2026-01-17 23:41:46 +0530

    executor implemented
    keyboard driver async

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,12 @@
 #![test_runner(zero::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
+extern crate alloc;
+
 use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
 use zero::println;
-extern crate alloc;
-
-use zero::task::{simple_executor::SimpleExecutor, Task};
+use zero::task::{executor::Executor, keyboard, Task};
 
 entry_point!(kernel_main);
 fn kernel_main(_boot_info: &'static BootInfo) -> ! {
@@ -18,7 +18,7 @@ fn kernel_main(_boot_info: &'static BootInfo) -> ! {
     use zero::memory;
     use zero::memory::BootInfoFrameAllocator;
 
-    println!("Welcome to ZERO OS{}", "!");
+    println!("ZERO OS");
     zero::init();
 
     let phys_mem_offset = VirtAddr::new(_boot_info.physical_memory_offset);
@@ -27,24 +27,12 @@ fn kernel_main(_boot_info: &'static BootInfo) -> ! {
 
     allocator::init_heap(&mut mapper, &mut frame_allocator).expect("heap inititalization failed");
 
-    let mut executor = SimpleExecutor::new();
-    executor.spawn(Task::new(example_task()));
-    executor.run();
-
     #[cfg(test)]
     test_main();
 
-    //println!("It did not crash!");
-    zero::hlt_loop();
-}
-
-async fn async_number() -> u32 {
-    42
-}
-
-async fn example_task() {
-    let number = async_number().await;
-    println!("async number: {}", number);
+    let mut executor = Executor::new();
+    executor.spawn(Task::new(keyboard::print_keypresses()));
+    executor.run();
 }
 
 // This function is called on panic.
